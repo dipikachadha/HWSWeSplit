fb0ab4514c378952673489376907b638511051e6
Dipika Chadha <dipika.chadha@gmail.com>
2021-07-09 11:05:42 -0400

    Initial Commit

---
+++ Shared/HWSWeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  HWSWeSplitApp.swift
+//  Shared
+//
+//  Created by Dipika Chadha on 7/9/21.
+//
+
+import SwiftUI
+
+@main
+struct HWSWeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
