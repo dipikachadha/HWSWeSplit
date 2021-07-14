3ef194df4880eeb2309deeff5f735d595085b5b8
Dipika Chadha <dipika.chadha@gmail.com>
2021-07-14 00:21:51 -0400

    Add basic variables and first cut form to WeSplit app

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -8,46 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
-    // structs can be constants, so having
-    // mutable properties is a no go. The workaround
-    // is to add @State wrapper for simple properties.
-    @State private var tapCount = 0
-    @State private var name = ""
-    let students = ["Harry", "Hermione", "Ron"]
-    @State private var selectedStudent = "Harry"
-
-    // The `some` here constraints that
-    // it is a single type of View, not
-    // any generic View
+    @State private var checkAmount = "2"
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 2
+    
+    let tipPercentages = [10, 15, 20, 25, 0]
+    
     var body: some View {
-        NavigationView {
-            Form {
-                Text("Hello, world!")
-                
-                Button("Tap Count: \(tapCount)") {
-                    self.tapCount += 1
-                }
-                
-                // The $ sign suggests that it's a
-                // 2-way binding. Values will be read
-                // from and updated into the same
-                // variable upon edits.
-                TextField("Enter your name", text: $name)
-                Text("Your name is \(name)")
-                
-                // Now for generating views in loops
-                // Use ForEach in this Picker:
-                Picker("Select your student", selection: $selectedStudent) {
-                    ForEach(0 ..< students.count) {
-                        Text(self.students[$0])
-                    }
-                }
-                Text("You chose: \(selectedStudent)")
-                
+        Form {
+            Section {
+                TextField("Amount", text: $checkAmount)
+                    .keyboardType(.decimalPad)
             }
-            .navigationBarTitle("SwiftUI", displayMode: .inline)
             
-
+            Section {
+                Text("$\(checkAmount)")
+            }
         }
     }
 }
